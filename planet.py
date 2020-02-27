78b7c27984b2f9b11c8afa7c2b42d21dd6f70983
Tnelsonn <topher4403@gmail.com>
2020-02-27 09:43:49 -0600

    planet class

--- planet.py
+++ planet.py
@@ -1,4 +1,13 @@
-import wikipedia
+class planet(object):
+    def __init__(self, name, mass, temp, radius, distance):
+        self.name = name
+        self.mass = mass
+        self.temp = temp
+        self.radius = radius
+        self.distance = distance
 
-class Planetarium
+    def __str__(self):
+        return f"Name: {self.name}, Mass: {self.mass}, Temp: {self.temp}, Radius: {self.radius}, Distance: {self.distance}"
 
+    
+    
