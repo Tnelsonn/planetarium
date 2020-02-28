72a5818d894429550674eec267efb9c911c24fb6
Tnelsonn <topher4403@gmail.com>
2020-02-28 08:44:33 -0600

    planet

--- planetpt2.py
+++ planetpt2.py
@@ -1,5 +1,18 @@
+
 from planet import planet
+Mars = planet("Mars ", "6.4171×1023 kg", " -82 F", "1000ft", "141.6 million mi")
+Earth = planet("Earth", "5.972 * 10^24 Kg","70 F", "3389.5 Km"," 92.96 million mi")
+Jupiter = planet("Jupiter", "1.898 × 10^27 kg (317.8 M⊕)","108 C","43,441 mi","483.8 million mi")
+Neptune = planet("Neptune", "1.024 × 10^26 kg (17.15 M⊕)", "-201 C", "15,299 mi", "2.793 billion mi")
 
-neptune = planet("Neptune", "69 Kg", "420 F", "1000ft", "4000km")
+print("What planet would you like to see?")
+p = input()
 
-print(str(neptune))
+if(p == "Mars"):
+    print(Mars)
+if(p == "Earth"):
+    print(Earth)
+if(p == "Jupiter"):
+    print(Jupiter)
+if(p == "Neptune"):
+    print(Neptune)
